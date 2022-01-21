a7ad5a40df33f87c809ff6bdd05b3b19977b9668
Rituparna Chakraborty <crituparna77@gmail.com>
2022-01-21 20:29:28 +0530

    initial push

---
+++ sketch.js
@@ -0,0 +1,63 @@
+var oceanImg, ocean;
+var coinImg, coin, coinGroup;
+var climberImg, climber, climbersGroup;
+var frog, frogImg;
+var gameState = "play"
+var score = 0;
+
+function preload(){
+  oceanImg = loadImage("water.jpg");
+  coinImg = loadImage("coin.png");
+  climberImg = loadImage("seaweed.png");
+  frogImg = loadImage("frog.png");
+  
+}
+
+function setup(){
+  createCanvas(600,450);
+  
+  ocean = createSprite(300,300);
+  ocean.addImage("ocean",oceanImg);
+  ocean.velocityY=-1.5;
+  frog = createSprite(200,200,50,50);
+  frog.scale = 0.1;
+  frog.addImage("frog", frogImg);  
+  
+  //create coin group and climber group
+  climbersGroup = new Group();
+  coinGroup = new Group();
+
+  score=0;
+}
+
+function draw(){
+  background(0);
+  drawSprites();
+  
+  if (gameState === "play") {
+  
+    
+  }
+  
+  if (gameState === "end"){
+    
+
+  }
+  //infinity background
+  if(ocean.position.y < 200){
+    ocean.position.y = 300;
+    }
+
+}
+
+// create the coin and climber in the same function
+function spawnCoin() {
+  
+  if (frameCount % 280 === 0) {
+    //make the x position of the coin and climber the same
+    coin = createSprite(Math.round(random(10,750)),80,200,50);
+    coin.addImage("coin",coinImg);
+    coinImg.setVelocity(0,3);
+  }
+}
+
